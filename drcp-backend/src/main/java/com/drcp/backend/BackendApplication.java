8c9ecd79db451d1c9e55ce25cdc2833a0b2730e1
sauashwork <onlycoding143@gmail.com>
2025-07-25 17:40:50 +0530

    trying docker fix 2

--- drcp-backend/src/main/java/com/drcp/backend/BackendApplication.java
+++ drcp-backend/src/main/java/com/drcp/backend/BackendApplication.java
@@ -1,6 +1,5 @@
 package com.drcp.backend;
 
-import com.drcp.backend.configs.DotenvInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		DotenvInitializer.loadEnv();
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
